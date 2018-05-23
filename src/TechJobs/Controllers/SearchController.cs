1a68825041aa821d1001a4685ff211286751b6d5
Earl <earljg@gmail.com>
2018-05-23 18:57:59 -0500

    created IActionResult Results

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,11 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
-
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+            
+            return View(searchResults);
+        }
     }
 }
